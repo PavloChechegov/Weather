12991dce95b920a38e5abf6f4c1c5b96462ba64f
paulch1991 <paulch1991@gmail.com>
2016-02-07 23:57:09 +0200

    add setWeatherIcon to show icon in screen

--- app/src/main/java/com/example/weather/app/MainActivity.java
+++ app/src/main/java/com/example/weather/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.weather.app;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.github.pwittchen.weathericonview.WeatherIconView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,49 +27,84 @@ import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mEnterCity;
-    private Button mButtonSearch;
-    private DownloadsWeather mWeather;
+    public Button mButtonSearch;
+    public DownloadsWeather mWeather;
     private TextView mResultTextView;
-    private HttpURLConnection httpURLConnection;
+    public HttpURLConnection httpURLConnection;
+    public WeatherIconView mWeatherIconView;
 
-    //when you click for button "Search" this method being invoked;
+    //method that give me int to resource object;
+    public int setWeatherIcon(int idIcon) {
+        int id = idIcon / 100;
+        int icon;
+        if (idIcon == 800) {
+            icon = R.string.wi_wu_sunny;
+        } else {
+            switch (id) {
+                case 2:
+                    icon = R.string.wi_thunderstorm;
+                    break;
+                case 3:
+                    icon = R.string.wi_fog;
+                    break;
+                case 7:
+                    icon = R.string.wi_wu_hazy;
+                    break;
+                case 8:
+                    icon = R.string.wi_cloudy;
+                    break;
+                case 6:
+                    icon = R.string.wi_snow;
+                    break;
+                case 5:
+                    icon = R.string.wi_rain_wind;
+                    break;
+                default:
+                    icon = R.string.wi_fire;
+            }
+        }
+
+        return icon;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.container);
 
         mEnterCity = (EditText) findViewById(R.id.etCity);
         mResultTextView = (TextView) findViewById(R.id.tvResult);
 
+
+        mWeatherIconView = (WeatherIconView) findViewById(R.id.ivIconWeather);
+        mWeatherIconView.setIconResource(getString(R.string.wi_day_sunny_overcast));
         mButtonSearch = (Button) findViewById(R.id.search_button);
         mButtonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("Click on button", mEnterCity.getText().toString());
 
+                //encode our text that we typing in editText field, if we have some space or coma or something like that
+                // this text we encoding for standard UTF-8
                 InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 manager.hideSoftInputFromWindow(mEnterCity.getWindowToken(), 0);
 
                 try {
-                    String encodeCityName = URLEncoder.encode(mEnterCity.getText().toString(),"UTF-8");
-
+                    //in background thread take weather data from this url
+                    String encodeCityName = URLEncoder.encode(mEnterCity.getText().toString(), "UTF-8");
                     mWeather = new DownloadsWeather();
                     mWeather.execute("http://api.openweathermap.org/data/2.5/weather?q=" +
                             encodeCityName + "&appid=eee8ded8ee45a1c5e98a4da3e86a896f");
-
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-
-
     }
 
-    public class DownloadsWeather extends AsyncTask<String, Void, String>{
+    public class DownloadsWeather extends AsyncTask<String, Void, String> {
+        public WeatherOfDay weatherOfDay;
 
         @Override
         protected String doInBackground(String... urls) {
@@ -75,38 +112,29 @@ public class MainActivity extends AppCompatActivity {
 
 
             try {
-
                 URL url = new URL(urls[0]);
                 httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.connect();
 
                 String line;
-                StringBuffer stringBuffer = new StringBuffer();
+                StringBuffer buffer = new StringBuffer();
 
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
 //                int data = reader.read();
-
-                while ((line = reader.readLine()) != null){
-                    stringBuffer.append(line);
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
                     /* why we doing something like that, i can't understand
                         because its not normal, we must create String or in best
                         case StringBuffer and add to this our reader. I think so!
                     char current = (char) data;
                     result += current;
                     data = reader.read();*/
-
-
                 }
 
-
-                resultJSON = stringBuffer.toString();
+                resultJSON = buffer.toString();
                 Log.i("Website content", resultJSON);
-
-
-
-
 
 
             } catch (MalformedURLException e) {
@@ -128,32 +156,32 @@ public class MainActivity extends AppCompatActivity {
             //Log.i("Website content", result);
             try {
 
-                String message = null;
-
                 JSONObject jsonObject = new JSONObject(result);
                 String weatherInfo = jsonObject.getString("weather");
                 Log.i("Weather content", weatherInfo);
 
                 JSONArray jsonArray = new JSONArray(weatherInfo);
-                for(int i = 0; i < jsonArray.length(); i++){
+                for (int i = 0; i < jsonArray.length(); i++) {
 
                     JSONObject object = jsonArray.getJSONObject(i);
+                    weatherOfDay = new WeatherOfDay(object);
 
-                    String main = object.getString("main");;
-                    String description = object.getString("description");;
+//                    String main = object.getString("main");;
+//                    String description = object.getString("description");
+//                    int icon = object.getInt("icon");
+                    if (weatherOfDay != null) {
+                        mResultTextView.setText(weatherOfDay.toString());
 
+                        //this line what doing, explain: 1. I get weather id from JSONObject(it can be 800, 801, 500, 400...)
+                        //and put to method setWeatherIcon(id) take from this method int reference to resources file
+                        // and put to method setIconResource. Its very difficult process but ut works
+                        mWeatherIconView.setIconResource(getString(setWeatherIcon(weatherOfDay.getId())));
+//
+                        Log.i("Weather", weatherOfDay.toString() + weatherOfDay.getIconView());
 
-                    if(main != null && description != null){
-                        message = main + ": " + description +"\r\n" ;
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_SHORT).show();
                     }
-                    Log.i("Message", message);
-
-                    if(message != null){
-                         mResultTextView.setText(message);
-
-                    } else Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
-
-
 
                     Log.i("Main: ", object.getString("main"));
                     Log.i("Description", object.getString("description"));
@@ -163,6 +191,7 @@ public class MainActivity extends AppCompatActivity {
 
             } catch (JSONException e) {
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_SHORT).show();
             }
 
 
--- app/src/main/java/com/example/weather/app/WeatherOfDay.java
+++ app/src/main/java/com/example/weather/app/WeatherOfDay.java
@@ -9,11 +9,18 @@ import org.json.JSONObject;
  */
 public class WeatherOfDay {
 
-    public int mIconView;
+    public String mIconView;
     public String mMainTitle;
     public String mDescription;
+    public int mId;
 
-    public WeatherOfDay(String mainTitle, String description, int iconView) {
+    @Override
+    public String toString() {
+        return mMainTitle +
+                ": " + mDescription;
+    }
+
+    public WeatherOfDay(String mainTitle, String description, String iconView) {
         mMainTitle = mainTitle;
         mDescription = description;
         mIconView = iconView;
@@ -22,7 +29,39 @@ public class WeatherOfDay {
     public WeatherOfDay(JSONObject object) throws JSONException {
             mMainTitle = object.getString("main");
             mDescription = object.getString("description");
-            mIconView = object.getInt("icon");
+            mIconView = object.getString("icon");
+            mId = object.getInt("id");
     }
 
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getIconView() {
+        return mIconView;
+    }
+
+    public void setIconView(String iconView) {
+        mIconView = iconView;
+    }
+
+    public String getMainTitle() {
+        return mMainTitle;
+    }
+
+    public void setMainTitle(String mainTitle) {
+        mMainTitle = mainTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
 }
