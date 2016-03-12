e2a1d09f9aaca472d3810596a4963c00ab7bacd6
paulch1991 <paulch1991@gmail.com>
2016-03-12 23:11:35 +0200

    add 2 button Weather and Location, and  interface that have 2 method that returning String CityName

--- app/src/main/java/com/example/weather/app/SearchFragment.java
+++ app/src/main/java/com/example/weather/app/SearchFragment.java
@@ -1,24 +1,72 @@
 package com.example.weather.app;
 
-import android.app.Fragment;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by pasha on 2/7/16.
  */
 public class SearchFragment extends Fragment {
 
+    public static final String SEARCH_TAG = "SearchFragment";
+    public EditText mEnterCity;
+    public Button mButtonSearch, mGetLocation;
+    public TextView textViewDescription;
 
-    public SearchFragment() {
+    OnClickSearchListener mOnClickSearchListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mOnClickSearchListener = (OnClickSearchListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement OnClickSearchListener");
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        return inflater.inflate(R.layout.activity_main, container, false);
+        mEnterCity = (EditText) view.findViewById(R.id.etCity);
+        mButtonSearch = (Button) view.findViewById(R.id.button_search);
+        textViewDescription = (TextView) view.findViewById(R.id.tvDescription);
+        mButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("CityName", mEnterCity.getText().toString());
+                String s = mEnterCity.getText().toString();
+                mOnClickSearchListener.cityName(s);
+            }
+        });
+
+        mGetLocation = (Button) view.findViewById(R.id.button_location);
+        mGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Location", mEnterCity.getText().toString());
+                String s = mEnterCity.getText().toString();
+                mOnClickSearchListener.getLocation(s);
+            }
+        });
+
+        return view;
     }
+
+    interface OnClickSearchListener {
+        void cityName(String city);
+        void getLocation(String city);
+    }
+
 }
